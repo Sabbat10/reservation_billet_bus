c3c28cf95f7ef9e3c84e7d2f73e41ead0395102d
= <=>
2025-03-12 20:09:36 +0100

    feat: add rechercher_voyage

--- bus_reservation.py
+++ bus_reservation.py
@@ -32,7 +32,18 @@ bus_disponible = [
 print("==" * 40)
 print("== Voici la liste des voyages disponibles ! ==")
 
+# afficher la liste des voyayes
 def afficher_liste_de_voyage_disponible():
     for index, trajet in enumerate(bus_disponible):
         print(f"{index + 1} - {trajet['ville_de_depart'].capitalize()} - {trajet['ville_d_arriver'].capitalize()} : Horaire 1.Matin {trajet['horaire_1']} 2.Midi {trajet['horaire_2']}")
         
+
+# recherher un yoyage disponible
+def rechercher_voyage(ville_depart, ville_arriver):
+    for index, trajet in enumerate(bus_disponible):
+        if ville_depart.strip().lower() == trajet['ville_de_depart'].strip().lower() and ville_arriver.strip().lower() == trajet['ville_d_arriver'].strip().lower():
+            print("")
+            print(f"Trajet : {trajet['ville_de_depart'].capitalize()} - {trajet['ville_d_arriver'].capitalize()} est disponible")
+            print(f"Horaires disponibles : Matin {trajet['horaire_1']} - Midi {trajet['horaire_2']}"  )
+            return
+    print("Trajet non disponible pour le moment")
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from bus_reservation import afficher_liste_de_voyage_disponible
+from bus_reservation import afficher_liste_de_voyage_disponible, rechercher_voyage
 
 while True:
+   print("")
    choix_operation = input("1 - Voir les voyages disponibles \n2 - Chercher un voyages\n4 - quitter \nchoix  : ")
+   print("")
    
    if choix_operation == "1":
        afficher_liste_de_voyage_disponible()
+       
+   elif choix_operation == "2":
+       depart = input("Entrer ville de depart : ")
+       arriver = input("Entrer ville d'arriver : ")
+       
+       rechercher_voyage(depart, arriver)
        
    elif choix_operation == "4":
        print("Fin du programme !")
