c0f9d8b45de015c14bcc8481508483e2b24a3eee
= <=>
2025-03-13 09:47:11 +0100

    feat: add reservation siege

--- bus_reservation.py
+++ bus_reservation.py
@@ -46,4 +46,14 @@ def rechercher_voyage(ville_depart, ville_arriver):
             print(f"Trajet : {trajet['ville_de_depart'].capitalize()} - {trajet['ville_d_arriver'].capitalize()} est disponible")
             print(f"Horaires disponibles : Matin {trajet['horaire_1']} - Midi {trajet['horaire_2']}"  )
             return
+    print("")
     print("Trajet non disponible pour le moment")
+    
+
+# reservation de siege
+
+def reservation_siege():
+    for i in range(1, 22):
+        print(f"{i}🪑", end=' ')
+        if i % 5 == 0:
+            print()  # Revenir à la ligne après chaque groupe de 5 nombres
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from bus_reservation import afficher_liste_de_voyage_disponible, rechercher_voyage
+from bus_reservation import afficher_liste_de_voyage_disponible, rechercher_voyage, reservation_siege
 
 while True:
    print("")
-   choix_operation = input("1 - Voir les voyages disponibles \n2 - Chercher un voyages\n4 - quitter \nchoix  : ")
+   choix_operation = input("1 - Voir les voyages disponibles \n2 - Chercher un voyages\n3 - Reserver un siege\n4 - quitter \nchoix  : ")
    print("")
    
    if choix_operation == "1":
@@ -13,7 +13,22 @@ while True:
        arriver = input("Entrer ville d'arriver : ")
        
        rechercher_voyage(depart, arriver)
+    
+   elif choix_operation == "3":
        
+       print("== Voici les siege disponible ==")
+       reservation_siege()
+       
+       
+       print("")
+       choisir_siege = input("choisir un siege : ")
+       
+       if choisir_siege.isdigit() and 1 <= int(choisir_siege) <= 21:
+           print(f"vous avez choisit le siege : {choisir_siege}🪑 ", )
+       else:
+           print("La siege n'est pas disponible !")
+       
+    
    elif choix_operation == "4":
        print("Fin du programme !")
        break
