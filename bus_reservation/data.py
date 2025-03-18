73bfe0bed850ea37a0616b83da1dddfaba0ea669
= <=>
2025-03-18 11:06:09 +0100

    delete file

--- bus_reservation/data.py
+++ bus_reservation/data.py
@@ -7,14 +7,14 @@ bus_disponible = [
         "horaire_2": "14h00"
     },
     
-     {
+    {
         "ville_de_depart" : "matadi",
         "ville_d_arriver": "Kinshasa",
         "horaire_1":  "08h00",
         "horaire_2": "14h00"
     },
      
-      {
+    {
         "ville_de_depart" : "kinshasa",
         "ville_d_arriver": "bandundu",
         "horaire_1":  "08h00",
