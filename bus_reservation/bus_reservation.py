9a4959414b43465589e5e397bab96aa106ad1b12
= <=>
2025-03-18 12:50:07 +0100

    feat: review code

--- bus_reservation/bus_reservation.py
+++ bus_reservation/bus_reservation.py
@@ -13,7 +13,14 @@ def afficher_liste_de_voyage_disponible():
 # recherher un yoyage disponible
 def rechercher_voyage(ville_depart, ville_arriver):
     for index, trajet in enumerate(bus_disponible):
-        if ville_depart.strip().lower() == trajet['ville_de_depart'].strip().lower() and ville_arriver.strip().lower() == trajet['ville_d_arriver'].strip().lower():
+        
+        # convertir les ville en minuscule
+        ville_depart.strip().lower()
+        ville_arriver.strip().lower()
+        trajet['ville_de_depart'].strip().lower()
+        trajet['ville_d_arriver'].strip().lower()
+        
+        if ville_depart == trajet['ville_de_depart'] and ville_arriver == trajet['ville_d_arriver']:
             print("")
             print(f"Trajet : {trajet['ville_de_depart'].capitalize()} - {trajet['ville_d_arriver'].capitalize()} est disponible")
             print(f"Horaires disponibles : Matin {trajet['horaire_1']} - Midi {trajet['horaire_2']}"  )
@@ -31,13 +38,11 @@ def reservation_siege():
             print()  # Revenir à la ligne après chaque groupe de 5 nombres
 
 # reservation bus
-
 def reservation_bus(ville_depart, ville_arriver):
     for index, trajet in enumerate(bus_disponible):
-        if ville_depart.strip().lower() == trajet['ville_de_depart'].strip().lower() and ville_arriver.strip().lower() == trajet['ville_d_arriver'].strip().lower():
-            print("")
-            print(f"Vous avez choisi de voyager de : {trajet['ville_de_depart'].capitalize()} à {trajet['ville_d_arriver'].capitalize()}")
-            return
+        rechercher_voyage(ville_depart, ville_arriver)
+        print(f"Vous avez choisi de voyager de : {trajet['ville_de_depart'].capitalize()} à {trajet['ville_d_arriver'].capitalize()}")
+        return
     print("")
     print("Trajet non disponible pour le moment")
     print("== Voici la liste des voyages disponibles ! ==")
@@ -53,12 +58,15 @@ def generer_facture(nom, prenom, age, depart, arriver, choisir_siege):
         "ville_d_arriver": "Ville d'arriver",
         "siege": "Siege choisit"
     }
-    facture = tabulate([{
+    
+    data = {
         "nom": nom,
         "prenom": prenom,
         "age": age,
         "ville_de_depart": depart,
         "ville_d_arriver": arriver,
         "siege": choisir_siege
-    }], headers=header, tablefmt="grid")
+    }
+    
+    facture = tabulate(data, headers=header, tablefmt="grid")
     return facture
